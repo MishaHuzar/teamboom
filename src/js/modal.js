ab6356455dfa4cb1adfc20943c92ab94671b70e1
Volodymyr "Kaiser <kaiser.mail28@gmail.com>
2025-07-26 23:03:28 +0300

    fix js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,7 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open-10]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
+      closeModalBtn: document.querySelector("[data-modal-close-10]"),
       modal: document.querySelector("[data-modal-10]"),
     };
   
@@ -22,7 +22,7 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open-65]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
+      closeModalBtn: document.querySelector("[data-modal-close-65]"),
       modal: document.querySelector("[data-modal-65]"),
     };
   
@@ -43,7 +43,7 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open-80]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
+      closeModalBtn: document.querySelector("[data-modal-close-80]"),
       modal: document.querySelector("[data-modal-80]"),
     };
   
